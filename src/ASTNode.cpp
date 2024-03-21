9c8dee019bba68137e0d06d0b6269fa4ca34eef2
euler <3149512122@qq.com>
2024-03-21 11:35:27 +0800

    分析语法，构建ASTNode文件

---
+++ src/ASTNode.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file ASTNode.cpp
+ * @author Euler_lne 
+ * @brief 抽象语法分析树的方法实现
+ * @version 0.1
+ * @date 2024-03-21
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
--- src/ASTNode.h
+++ src/ASTNode.h
@@ -1,6 +1,6 @@
 /**
  * @file ASTNode.h
- * @author Euler_lne (Euler_lne@outlook.com)
+ * @author Euler_lne
  * @brief 构建抽象语法数的头文件
  * @version 0.1
  * @date 2024-03-21
