da8ef1cc4e4f057b60fa50d7fea3b3dfbf6e9d6b
Euler-lne <319512122@qq.com>
2024-03-23 21:14:31 +0800

    完善Token

--- include/TokenTypeEnum.h
+++ include/TokenTypeEnum.h
@@ -45,9 +45,6 @@ namespace Token
         ELSE_PART_,
         // CASE 语句
         CASE_BODY_,
-        BRANCH_LIST_,
-        BRANCH_,
-        CONST_LIST,
         // FOR
         UPDOWN_,
         // 赋值语句
@@ -100,13 +97,22 @@ namespace Token
         MULOP,    // “*”、“/"'、"div”、"mod”和“and”
         ASSIGNOP, // “:—”
 
+        DOT,                      // .
+        RANGE_DOT,                //..
+        COMMA,                    // ,
+        EQUAL,                    // =
+        LEFT_PARENTHESES,         //(
+        RIGHT_PARENTHESES,        // )
+        LEFT_MEDIUM_PARENTHESES,  // [
+        RIGHT_MEDIUM_PARENTHESES, // ]
+        SEMICOLON,                // ;
+        ADD,                      // +
+        MINUS,                    //-
+
         ID,        // ID
         INT_NUM,   // 整数
         FLOAT_NUM, // 浮点数
         LETTER,    // 字符串
-
-        DOT,      // .
-        RANGEDOT, //..
 
         // 一下类型供构建抽象语法树以及之后的步骤使用
         NULL, // 添加一个返回值类型，代表这个为一个记录类型
