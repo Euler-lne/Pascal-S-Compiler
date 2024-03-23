83b2aca20ee8d88fed52f93825f58b9315c49d0c
euler <3149512122@qq.com>
2024-03-23 09:56:45 +0800

    更改头文件路径

--- lex_yacc_src/main.h
+++ lex_yacc_src/main.h
@@ -9,7 +9,7 @@ class ParseNode
 {
 public:
     string val;                   // 终结符号的具体属性值，非终结符号没有改属性
-    string token;                 // 终结符号或非终结符号本身的名称
+    string token;                 // 终结符号或非终结符号本身的名称，这里用常量表示
     int lineNumber;               // 终结符号的行号，用于之后错误处理报错行数
     vector<ParseNode *> children; // 该节点对应的子节点
 
