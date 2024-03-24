e62a075663e16f1a3ea937179a61db0a0c8ea321
z_8_3_8 <z_8_3_8@163.com>
2024-03-24 21:20:23 +0800

    [zyq]add file CodeGenerration.cpp ~.h

---
+++ include/CodeGeneration.h
@@ -0,0 +1,17 @@
+/**
+ * @file CodeGeneration.h
+ * @author z_8_3_8
+ * @brief AST To C Code
+ * @version 0.0.1
+ * @date 2024-03-24
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#pragma once;
+
+namespace C_GEN
+{
+
+} // namespace C_GEN
