b39e5948b3f9905016ce05045e485c70588909c9
z_8_3_8 <z_8_3_8@163.com>
2024-03-25 15:47:51 +0800

    [zyq]impro file CodeGenerration.cpp ~.h

--- include/CodeGeneration.h
+++ include/CodeGeneration.h
@@ -13,6 +13,7 @@
 #include <iostream>
 #include <string>
 #include "TokenTypeEnum.h"
+#include "ASTNode.h"
 
 namespace C_GEN
 {
@@ -21,13 +22,22 @@ namespace C_GEN
     private:
         std::string outPutPath;
         Token::GenerationType generationType;
+        AST::Program *ast;
 
     public:
         C_Generater();
-        C_Generater(std::string _filePath, Token::GenerationType _generationType = Token::GenerationType::C) : outPutPath(_filePath), generationType(_generationType){};
+        C_Generater(AST::Program *pAST, std::string _filePath, Token::GenerationType _generationType = Token::GenerationType::C) : ast(pAST), outPutPath(_filePath), generationType(_generationType){};
         ~C_Generater();
 
         void SetPath(std::string _filePath) { outPutPath = _filePath; };
         void SetGenType(Token::GenerationType _generationType) { generationType = _generationType; };
+        void SetAST(AST::Program *pAST) { ast = pAST; };
+
+        void run();
+
+    private:
+        void C_Generate();
+        void PY_Generate(){};
+        void JAVA_Generate(){};
     };
-} // namespace C_GEN
+} // namespace C_G
--- src/CodeGeneration.cpp
+++ src/CodeGeneration.cpp
@@ -15,9 +15,36 @@ namespace C_GEN
 {
     C_Generater::C_Generater()
     {
+        this->ast = nullptr;
         this->outPutPath = "";
         this->generationType = Token::GenerationType::C;
     }
 
     C_Generater::~C_Generater(){};
+
+    void C_Generater::run()
+    {
+        if (ast == nullptr)
+        {
+            std::cout << "ERROR::No AST For Generater" << std::endl;
+            return;
+        }
+
+        if (this->generationType == Token::GenerationType::C)
+        {
+            C_Generate();
+        }
+        else if (this->generationType == Token::GenerationType::JAVA)
+        {
+            JAVA_Generate();
+        }
+        else if (this->generationType == Token::GenerationType::PYTHON)
+        {
+            PY_Generate();
+        }
+    }
+
+    void C_Generater::C_Generate()
+    {
+        }
 };
