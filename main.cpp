d41ae19c5227b04571170ec056e950d0658665c0
gllxgllx <202555941@qq.com>
2024-04-04 19:33:02 +0800

    lex修改+唐宇凡更改main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ string itos(int num)
 int main()
 {
     // blog.csdn.net/cscmaker/article/details/7042718
-    string inName = "../../test/error1.pas"; // 默认输入文件名
+    string inName = "../../test/1.pas"; // 默认输入文件名
     FILE *fp = NULL;
     fp = fopen(inName.c_str(), "r");
     if (fp == NULL) {
