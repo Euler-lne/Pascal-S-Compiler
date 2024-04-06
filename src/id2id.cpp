2286951746a4ed8013b7847f425451253eef3c7f
zhangshengzhezsz <3152532499@qq.com>
2024-04-06 18:08:31 +0800

    id2id.h

--- src/id2id.cpp
+++ src/id2id.cpp
@@ -1,25 +1,35 @@
 #include "id2id.h"
 
-std::string NameManager::insert(const std::string& idName) {
+std::string id_management::NameManager::insert(const std::string &idName)
+{
     // 检查 idName 是否已经在 nameMap 中
     auto it = nameMap.find(idName);
-    if (it != nameMap.end()) {
-        // 如果找到了, 返回当前对应的 index
-        return "Trans" + it->second;
-    } else {
+    if (it != nameMap.end())
+    {
+        // 如果找到了, 说明重命名了，要报错
+        std::cerr << "Error: Redefinition of " << idName << std::endl;
+        // 直接exit
+        exit(-1);
+    }
+    else
+    {
         // 如果没找到, 添加新的 idName 并返回新的 index
-        std::string newIndex = "Trans" + std::to_string(currentIndex++);
+        std::string newIndex = "T" + std::to_string(currentIndex++);
         nameMap[idName] = newIndex;
         return newIndex;
     }
 }
 
-std::string NameManager::get(const std::string& idName) {
+std::string id_management::NameManager::get(const std::string &idName)
+{
     // 查找 idName 在 nameMap 中的 index
     auto it = nameMap.find(idName);
-    if (it != nameMap.end()) {
+    if (it != nameMap.end())
+    {
         return it->second;
-    } else {
+    }
+    else
+    {
         // 如果 idName 不存在, 返回空字符串
         return "";
     }
