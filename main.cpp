8cc99f02a82c236ae16fbddfcd37d827c46334e9
z_8_3_8 <z_8_3_8@163.com>
2024-04-21 21:49:57 +0800

    [zyq]nothing

--- main.cpp
+++ main.cpp
@@ -21,7 +21,8 @@ int main()
     string inName = "../../test/3.pas"; // 默认输入文件名
     FILE *fp = NULL;
     fp = fopen(inName.c_str(), "r");
-    if (fp == NULL) {
+    if (fp == NULL)
+    {
         cout << "Cannot open PASCAL-S file " << inName.c_str() << " , please check it." << endl;
         exit(0);
     }
@@ -29,12 +30,14 @@ int main()
     cout << "Now start lex and syntax analyse..." << endl;
     yyparse(); // 调用语法分析程序
     fclose(fp);
-    if (ERROR_NUM == 0) {
+    if (ERROR_NUM == 0)
+    {
         // --------- 进入语法分析 ---------
         AST::Program program(ParseTreeHead);
         reduceNode.Clear();
         delete ParseTreeHead;
-        if (ERROR_NUM == 0) {
+        if (ERROR_NUM == 0)
+        {
             // --------- 进入代码生成 ---------
             C_GEN::C_Generater gen(&program, "");
             gen.run();
