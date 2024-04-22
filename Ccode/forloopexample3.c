48a91643e0a4c85021e3a38816a9777d0d54239f
z_8_3_8 <z_8_3_8@163.com>
2024-04-22 11:21:16 +0800

    [zyq]

---
+++ Ccode/forloopexample3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define bool int
+#define true 1
+#define false 0
+int forloopexample3_i;
+int forloopexample3_j;
+int main()
+{
+for(forloopexample3_i = 1;
+forloopexample3_i <= 9;
+forloopexample3_i++)
+{
+{
+for(forloopexample3_j = 1;
+forloopexample3_j <= forloopexample3_i;
+forloopexample3_j++)
+{
+{
+printf("%d %s %d %s %f %s", forloopexample3_j, " * ", forloopexample3_i, " = ", forloopexample3_i * forloopexample3_j, "   ");
+}
+}
+}
+}
+}
