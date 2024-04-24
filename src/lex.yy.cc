85c551a73d44974bc8ddf5bad2e06ef2dda2fd25
gllxgllx <202555941@qq.com>
2024-04-24 17:03:52 +0800

    lex复原

--- src/lex.yy.cc
+++ src/lex.yy.cc
@@ -549,7 +549,7 @@ extern string itos(int num);
 
 
 string charRec; //保存字符常量
-char lineBuffer[6000]; //保存当前行的所有内容
+char lineBuffer[500]; //保存当前行的所有内容
 vector<string> lexicalErrorInformation;
 void addLexicalErrorInformation(char *word, string info, int l, int r);
 bool CheckAndAddLengthTooLargeErrorInformation(char *text, string type, int l, int r);
