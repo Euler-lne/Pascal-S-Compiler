b69c41428f727b618714e08172d230e138655ea7
zhangshengzhezsz <3152532499@qq.com>
2024-05-10 13:08:06 +0800

    test

---
+++ 自动测试比对脚本/C_code/1.c
@@ -0,0 +1,7 @@
+// C_code/no_input.c
+#include <stdio.h>
+
+int main() {
+    printf("This is a program with no input.");
+    return 0;
+}
---
+++ 自动测试比对脚本/P_code/1.pas
@@ -0,0 +1,4 @@
+program NoInput;
+begin
+    write('This is a program with no input.');
+end.
